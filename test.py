ef3d99242a6e5c935da363dc35fc1a0c8d9a44ee
Mamajonov Ibrohimjon <mamajonovibrokhimjon@gmail.com>
2023-12-25 15:06:50 +0500

    add test.py

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
-# a = '10001'
-# b = 5.67
-# print(int(a, base=2), int(b))
-# float
-# a = 12.1297835983498534987598735987635874583476548374
-# b = 12.1297835983498530000000000000000000000000000000
-# print(type(a), type(int(a)), int(12), a)
-# from decimal import Decimal
-# b = Decimal(str(a))
-# print(Decimal(1.12))
-# print(b)
+# a = 11
 
+# def b():
+#     a = 12
+#     def d():
+#         a = 13
+#         def f():
+#             global a
+#             a = 1
+#             print(a)
+#         f()
+#         print(a)
+#     d()
+#     print(a)
+
+# b()
+# print(a)
